11731fb2a6d1aea29b94448c8a3ba65e7cdd7a29
desco95 <l.davidescobedo15@gmail.com>
2025-12-11 01:09:37 -0600

    Update config.js

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,5 +1,5 @@
 // Configuración de la API
 // CAMBIAR ESTA URL DESPUÉS DE DESPLEGAR EL BACKEND
-const API_URL = "https://beautyweb-api.onrender.com";
+const API_URL = "https://beautyweb-3.onrender.com";
 
 window.API_URL = API_URL;
